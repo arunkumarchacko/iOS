3236bc78dad4748cee9484baef08d3eeea19f7ee
Arunkumar Chacko <arun@arunkumars-mac-mini.local>
2015-11-18 21:57:40 +0530

    MemeMe version1 submission

---
+++ Meme/Meme/Meme.swift
@@ -0,0 +1,76 @@
+//
+//  Meme.swift
+//  Meme
+//
+//  Created by Arunkumar Chacko on 17/11/15.
+//  Copyright © 2015 Chacko Apps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+internal class Meme {
+    let memeFileNamesKey = "MemeFileNames"
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    let memeImage: UIImage
+    
+    init(_ topText: String, _ bottomText: String, originalImage: UIImage, memeImage: UIImage) {
+        self.topText = topText
+        self.bottomText = bottomText
+        self.originalImage = originalImage
+        self.memeImage = memeImage
+    }
+    
+    func save() {
+        print("Saving image: Top=\(topText); Bottom=\(bottomText)")
+        
+        var memeFileNames = getSetting(memeFileNamesKey, defaultValue: [String]())
+        print("MemeFiles=\(memeFileNames)")
+
+        let f = getFileName()
+
+        writeJpegFile(getPathToSave(f, fileType: "original"))
+        writeJpegFile(getPathToSave(f, fileType: "meme"))
+        
+        memeFileNames.append(f)
+        setSetting(memeFileNamesKey, val: memeFileNames)
+    }
+    
+    func writeJpegFile(fileName: NSURL) {
+        print("Writing to file \(fileName.path)")
+        UIImageJPEGRepresentation(originalImage, 1.0)?.writeToFile(fileName.path!, atomically: true)
+    }
+    
+    func getPathToSave(name: String, fileType: String) -> NSURL {
+        let dirPath = NSSearchPathForDirectoriesInDomains(.PicturesDirectory, .UserDomainMask, true)[0] as String
+        let pathArray = [dirPath, "\(name)_\(fileType).jpg"]
+        return NSURL.fileURLWithPathComponents(pathArray)!
+    }
+    
+    func getFileName() -> String {
+        let currentDateTime = NSDate()
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "ddMMyyyy-HHmmss"
+        return formatter.stringFromDate(currentDateTime)
+    }
+    
+    func getSetting<T>(key : String, defaultValue : T) -> T {
+        let t = NSUserDefaults.standardUserDefaults()
+        
+        if let a: AnyObject = t.objectForKey(key) {
+            return a as! T
+        }
+        else {
+            return defaultValue
+        }
+    }
+
+    func setSetting(key : String, val : AnyObject)  {
+        print("Setting \(key) = \(val)")
+        let t = NSUserDefaults.standardUserDefaults()
+        t.setObject(val, forKey: key)
+        t.synchronize()
+    }
+}
--- Meme/Meme/MemeViewController.swift
+++ Meme/Meme/MemeViewController.swift
@@ -8,18 +8,160 @@
 
 import UIKit
 
-class MemeViewController: UIViewController {
-
+internal class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    @IBOutlet weak var imageView: UIImageView!    
+    @IBOutlet weak var topTextView: UITextField!
+    @IBOutlet weak var bottomTextview: UITextField!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var navigationBar: UINavigationItem!
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
+
+        set(topTextView)
+        set(bottomTextview)
+        
+        shareButton.enabled = false
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeNotifications()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeNotifications()
+    }
+    
+    @IBAction func onPickClicked(sender: UIBarButtonItem) {
+        let imgPicker = UIImagePickerController()
+
+        imgPicker.sourceType = sender.tag == 1 ? .PhotoLibrary: .Camera
+        imgPicker.delegate = self
+        
+        presentViewController(imgPicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func onShareClicked(sender: AnyObject) {
+        let memedImage = generateMemedImage()
+        let avc = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
+        presentViewController(avc, animated: true) { self.saveMeme(memedImage) }
+    }    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        print("Picked - \(info)")
+        
+        if let img = info[ UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = img
+            shareButton.enabled = true
+        }
+        
+        dismissImagePicker(picker)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        print("Picker cancelled")
+        dismissImagePicker(picker)
+    }
+    
+    func dismissImagePicker(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func subscribeNotifications() {
+        print("subscribeNotifications")
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:"    , name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:"    , name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeNotifications() {
+        print("unsubscribeNotifications")
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        print("Received - UIKeyboardWillShowNotification")
+        
+        if topTextView.editing {
+            // No need to reposition keyboard for top textview
+            return
+        }
+        
+        view.frame.origin.y -= getKeyboardHeight(notification)
     }
 
-
+    func keyboardWillHide(notification: NSNotification) {
+        print("Received - UIKeyboardWillHideNotification")
+        
+        if topTextView.editing {
+            // No need to reposition keyboard for top textview
+            return
+        }
+        
+        view.frame.origin.y += getKeyboardHeight(notification)
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().height
+    }
+    
+    func set(textField : UITextField) {
+        let memeTextAttributes = [
+            NSStrokeColorAttributeName : UIColor.blackColor(),
+            NSForegroundColorAttributeName : UIColor.blueColor(),
+            NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName : 5]
+        
+        textField.delegate = self
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.autocapitalizationType = UITextAutocapitalizationType.AllCharacters
+        textField.adjustsFontSizeToFitWidth = true
+        textField.textAlignment = .Center
+    }
+    
+    func generateMemedImage() -> UIImage
+    {
+        // So that Toolbar is not captures in the Meme image.
+        toolBar.hidden = true
+        
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.drawViewHierarchyInRect(view.frame, afterScreenUpdates: true)
+        let memedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        toolBar.hidden = false
+        
+        return memedImage
+    }
+    
+    func saveMeme(image: UIImage) {
+        print("Saving meme")
+        let m = Meme(topTextView.text!, bottomTextview.text!, originalImage: imageView.image!, memeImage: image)
+        m.save()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.text = ""
+        editing = true
+    }    
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        var newText = textField.text! as NSString
+        newText = newText.stringByReplacingCharactersInRange(range, withString: string.uppercaseString)
+        textField.text = newText as String
+        
+        return false
+    }
 }
 
